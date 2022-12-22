da8943212a42c2db736811f123dbd4569342d082
AhmedRazaSiddiqui <Ahmedsiddiqui551@gmail.com>
2022-12-22 19:01:07 +0500

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('create-message')
+  async handleUserCreated() {
+    // business logic
+    console.log('test micro service');
   }
 }
