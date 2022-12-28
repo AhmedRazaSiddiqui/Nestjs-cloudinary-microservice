fd9b3933fb999eb520d84671479d127bd3fc28c1
AhmedRazaSiddiqui <Ahmedsiddiqui551@gmail.com>
2022-12-28 19:51:16 +0500

    streams

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,9 @@ import {
   Get,
   Param,
   Post,
+  Req,
+  Res,
+  StreamableFile,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -11,6 +14,9 @@ import { EventPattern, MessagePattern } from '@nestjs/microservices';
 import { CloudinaryService } from './cloudinary/cloudinary.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import type { Response, Request } from 'express';
 
 @Controller()
 export class AppController {
@@ -30,6 +36,7 @@ export class AppController {
       storage: diskStorage({
         destination: 'upload-assets',
         filename: (_req, file, callback) => {
+          console.log(file);
           callback(null, file.originalname);
         },
       }),
@@ -37,6 +44,21 @@ export class AppController {
   )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return file.filename;
+  }
+
+  @Get('stream')
+  getFile(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ): StreamableFile {
+    const file = './upload-assets/test.mp4';
+    const readStream = createReadStream(file);
+    readStream.on('data', (chunk) => console.log(chunk));
+    readStream.on('finish', () => console.log('done'));
+    res.set({
+      'Content-Type': 'video/mp4',
+    });
+    return new StreamableFile(readStream);
   }
 
   @Get('retrieve-file-from-cloudinary/:id')
